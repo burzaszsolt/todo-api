545a5a301b4f5a3e144f01d3bc9911dade7142b6
Burzás Zsolt <zsolt.burzas@jaystack.com>
2019-03-08 15:58:15 +0100

    fixed endpoints

--- src/index.js
+++ src/index.js
@@ -1,118 +1,156 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs-extra');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs-extra");
+const uuidv1 = require("uuid/v1");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 const todos = express.Router();
-const file = 'todos.json';
+const FILE_NAME = "todos.json";
 
-// TODO: util függvény, ami getTodos néven fut, megpróbálja olvasni, és ha nem sikerül, akkor visszaad egy üres tömböt
+const getTodos = () => {
+  return fs
+    .readJson(FILE_NAME)
+    .then(todos => {
+      return todos;
+    })
+    .catch(err => {
+      return [];
+    });
+};
 
-todos.get('/', async (req, res) => {
-  try {
-    const todos = await fs.readJson(file);
-    res.json(todos);
-  } catch (error) {
-    res.send(error);
-  }
+todos.get("/", async (req, res) => {
+  const todos = await getTodos();
+  res.json(todos);
 });
 
 todos.get(
-  '/:id',
-  (req, res, next) => {
-    // TODO: ha nem parseolható az id, akkor dobjunk errort
-    req.params.id = Number(req.params.id);
-    next();
-  },
-  (req, res) => {
-    // TODO: használjunk mindenol promise interface-t, és kezeljünk hibát try-catch-csel
-    // TODO: hogyha nincs ilyen id-jú item, akkor 404-es status-t és errort küldjünk
-    // res.status(404).send(new Error('Baj van'))
-    fs.readJson(file, (err, todos) => {
-      const todo = todos.find(todo => todo.id === req.params.id);
-      res.json(todo);
-    });
+  "/:id",
+  // (req, res, next) => {
+  //   req.params.id = Number(req.params.id);
+  //   if (isNaN(req.params.id)) {
+  //     res.status(400).send(new Error("Wrong id"));
+  //     return;
+  //   }
+  //   next();
+  // },
+  async (req, res) => {
+    const todos = await getTodos();
+    if (todos.findIndex(todo => todo.id === req.params.id) === -1) {
+      res
+        .status(404)
+        .send(new Error("The requested resource could not be found."));
+      return;
+    }
+    const todo = todos.find(todo => todo.id === req.params.id);
+    res.json(todo);
   }
 );
 
-todos.post('/', async (req, res) => {
-  // TODO: ensure nem jó semmire
-  await fs.ensureFile(file);
-  const todosJson = await fs.readJson(file, { throws: false });
-  const todos = todosJson !== null ? todosJson : [];
-  // TODO: id-t adjon a szerver
-  // TODO: POST /todos térjen vissza teljes listával
-  fs.writeJSON(file, [...todos, req.body], err => {
-    if (err) res.send(err);
-    res.json({ success: true });
-  });
+todos.post("/", async (req, res) => {
+  const todos = await getTodos();
+  await fs
+    .writeJson(FILE_NAME, [...todos, { ...req.body, id: uuidv1() }])
+    .then(async () => {
+      const newTodos = await getTodos();
+      res.json(newTodos);
+    })
+    .catch(err => {
+      res.status(500).send(new Error(err));
+    });
 });
 
 todos.put(
-  '/:id',
-  (req, res, next) => {
-    req.params.id = Number(req.params.id);
-    next();
-  },
+  "/:id",
+  // (req, res, next) => {
+  //   req.params.id = Number(req.params.id);
+  //   if (isNaN(req.params.id)) {
+  //     res.status(400).send(new Error("Wrong id"));
+  //     return;
+  //   }
+  //   next();
+  // },
   async (req, res) => {
-    await fs.ensureFile(file);
-    const todosJson = await fs.readJson(file, { throws: false });
-    const todos = todosJson !== null ? todosJson : [];
-    // TODO: a body az egy delta // { ...todo, ...req.body }
-    const newTodos = todos.map(todo => (todo.id === req.params.id ? { ...todo, name: req.body.name } : todo));
-    fs.writeJSON(file, newTodos, err => {
-      if (err) res.send(err);
-      // TODO: visszaköltjük az adott todo-t
-      res.json({ success: true });
-    });
+    const todos = await getTodos();
+    if (todos.findIndex(todo => todo.id === req.params.id) === -1) {
+      res
+        .status(404)
+        .send(new Error("The requested resource could not be found."));
+      return;
+    }
+    const newTodos = todos.map(todo =>
+      todo.id === req.params.id ? { ...todo, ...req.body } : todo
+    );
+    await fs
+      .writeJson(FILE_NAME, newTodos)
+      .then(() => {
+        res.json(newTodos);
+      })
+      .catch(err => {
+        res.status(500).send(new Error(err));
+      });
   }
 );
 
-todos.delete('/all', async (req, res) => {
-  await fs.ensureFile(file);
-  fs.writeJSON(file, [], err => {
-    if (err) res.send(err);
-    // TODO: visszaküldjük, ami megmaradt
-    res.json({ success: true });
-  });
+todos.delete("/all", async (req, res) => {
+  await fs
+    .writeJson(FILE_NAME, [])
+    .then(() => {
+      res.json([]);
+    })
+    .catch(err => {
+      res.status(500).send(new Error(err));
+    });
 });
 
-todos.delete('/completed', async (req, res) => {
-  await fs.ensureFile(file);
-  const todosJson = await fs.readJson(file, { throws: false });
-  const todos = todosJson !== null ? todosJson : [];
+todos.delete("/completed", async (req, res) => {
+  const todos = await getTodos();
   const newTodos = todos.filter(todo => !todo.completed);
-  fs.writeJSON(file, newTodos, err => {
-    if (err) res.send(err);
-    // TODO: visszaküldjük, ami megmaradt
-    res.json({ success: true });
-  });
+  await fs
+    .writeJson(FILE_NAME, newTodos)
+    .then(() => {
+      res.json(newTodos);
+    })
+    .catch(err => {
+      res.status(500).send(new Error(err));
+    });
 });
 
 todos.delete(
-  '/:id',
-  (req, res, next) => {
-    req.params.id = Number(req.params.id);
-    next();
-  },
+  "/:id",
+  // (req, res, next) => {
+  //   req.params.id = Number(req.params.id);
+  //   if (isNaN(req.params.id)) {
+  //     res.status(400).send(new Error("Wrong id"));
+  //     return;
+  //   }
+  //   next();
+  // },
   async (req, res) => {
-    await fs.ensureFile(file);
-    const todosJson = await fs.readJson(file, { throws: false });
-    const todos = todosJson !== null ? todosJson : [];
+    const todos = await getTodos();
+    if (todos.findIndex(todo => todo.id === req.params.id) === -1) {
+      res
+        .status(404)
+        .send(new Error("The requested resource could not be found."));
+      return;
+    }
     const newTodos = todos.filter(todo => todo.id !== req.params.id);
-    fs.writeJSON(file, newTodos, err => {
-      if (err) res.send(err);
-      // TODO: visszaköltjük az adott todo-t
-      res.json({ success: true });
-    });
+    await fs
+      .writeJson(FILE_NAME, newTodos)
+      .then(() => {
+        res.json(newTodos);
+      })
+      .catch(err => {
+        res.status(500).send(new Error(err));
+      });
   }
 );
 
-app.use('/todos', todos);
+app.use("/todos", todos);
 
 app.listen(8000, err => {
-  if (!err) console.log('fut a szerver');
+  fs.ensureFile(FILE_NAME);
+  if (!err) console.log("fut a szerver");
 });
