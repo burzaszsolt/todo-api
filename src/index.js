327b4d0281d329fbbc67ef01c70642a82b6a9364
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2019-03-08 10:31:16 +0100

    overview

--- src/index.js
+++ src/index.js
@@ -9,18 +9,28 @@ app.use(bodyParser.json());
 const todos = express.Router();
 const file = 'todos.json';
 
+// TODO: util függvény, ami getTodos néven fut, megpróbálja olvasni, és ha nem sikerül, akkor visszaad egy üres tömböt
+
 todos.get('/', async (req, res) => {
-  const todos = await fs.readJson(file);
-  res.json(todos);
+  try {
+    const todos = await fs.readJson(file);
+    res.json(todos);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 todos.get(
   '/:id',
   (req, res, next) => {
+    // TODO: ha nem parseolható az id, akkor dobjunk errort
     req.params.id = Number(req.params.id);
     next();
   },
   (req, res) => {
+    // TODO: használjunk mindenol promise interface-t, és kezeljünk hibát try-catch-csel
+    // TODO: hogyha nincs ilyen id-jú item, akkor 404-es status-t és errort küldjünk
+    // res.status(404).send(new Error('Baj van'))
     fs.readJson(file, (err, todos) => {
       const todo = todos.find(todo => todo.id === req.params.id);
       res.json(todo);
@@ -29,9 +39,12 @@ todos.get(
 );
 
 todos.post('/', async (req, res) => {
+  // TODO: ensure nem jó semmire
   await fs.ensureFile(file);
   const todosJson = await fs.readJson(file, { throws: false });
   const todos = todosJson !== null ? todosJson : [];
+  // TODO: id-t adjon a szerver
+  // TODO: POST /todos térjen vissza teljes listával
   fs.writeJSON(file, [...todos, req.body], err => {
     if (err) res.send(err);
     res.json({ success: true });
@@ -48,9 +61,11 @@ todos.put(
     await fs.ensureFile(file);
     const todosJson = await fs.readJson(file, { throws: false });
     const todos = todosJson !== null ? todosJson : [];
+    // TODO: a body az egy delta // { ...todo, ...req.body }
     const newTodos = todos.map(todo => (todo.id === req.params.id ? { ...todo, name: req.body.name } : todo));
     fs.writeJSON(file, newTodos, err => {
       if (err) res.send(err);
+      // TODO: visszaköltjük az adott todo-t
       res.json({ success: true });
     });
   }
@@ -60,6 +75,7 @@ todos.delete('/all', async (req, res) => {
   await fs.ensureFile(file);
   fs.writeJSON(file, [], err => {
     if (err) res.send(err);
+    // TODO: visszaküldjük, ami megmaradt
     res.json({ success: true });
   });
 });
@@ -71,6 +87,7 @@ todos.delete('/completed', async (req, res) => {
   const newTodos = todos.filter(todo => !todo.completed);
   fs.writeJSON(file, newTodos, err => {
     if (err) res.send(err);
+    // TODO: visszaküldjük, ami megmaradt
     res.json({ success: true });
   });
 });
@@ -88,6 +105,7 @@ todos.delete(
     const newTodos = todos.filter(todo => todo.id !== req.params.id);
     fs.writeJSON(file, newTodos, err => {
       if (err) res.send(err);
+      // TODO: visszaköltjük az adott todo-t
       res.json({ success: true });
     });
   }
