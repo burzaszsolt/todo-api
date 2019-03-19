2f4106cad0c4b5486e4cfb6b27842129a73f3c93
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2019-03-19 14:29:04 +0100

    use cors package

--- src/index.js
+++ src/index.js
@@ -3,29 +3,13 @@ require('express-async-errors');
 const bodyParser = require('body-parser');
 const fs = require('fs-extra');
 const uuidv1 = require('uuid/v1');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// Add headers
-app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 const todos = express.Router();
 const FILE_NAME = 'todos.json';
